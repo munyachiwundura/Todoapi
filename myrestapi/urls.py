82a575492146dbd72f049a1ef64b7c4252ae2100
therealmunya <munyachiwundura1999@gmail.com>
2021-11-26 17:30:15 +0200

    tichichema

--- myrestapi/urls.py
+++ myrestapi/urls.py
@@ -23,5 +23,4 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("rest_framework.urls")),
     path("todoapi/", include("todoapi.urls")),
-    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
